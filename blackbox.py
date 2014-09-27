7bb857985f2f9b9de791239648002e310b688073
Lijie Huang <huanglijie.seal@gmail.com>
2014-09-27 22:55:05 +0800

    update

--- blackbox.py
+++ blackbox.py
@@ -4,10 +4,109 @@
 import os
 import time
 import numpy as np
+import re
 
 import autoroilib as arlib
 import macro
 
+def get_label_file(subject_dir):
+    """
+    Get a subject-specific label file.
+
+    """
+    f_list = os.listdir(subject_dir)
+    for f in f_list:
+        if re.search('_ff.nii.gz', f):
+            return os.path.join(subject_dir, f)
+        else:
+            return None
+
+def get_zstat_list(sid_list, db_dir):
+    """
+    Get a zstat file list based on sessid list and database directory.
+
+    """
+    zstat_list = []
+    for subj in sid_list:
+        subject_dir = os.path.join(db_dir, sid, 'face-object')
+        if not os.path.exists(subject_dir):
+            print 'Subject %s does not exist in database.'%(subj)
+            return
+        zstat = os.path.join(subject_dir, 'zstat1.nii.gz')
+        zstat_list.append(zstat)
+    return zstat_list
+
+def get_label_list(sid_list, db_dir):
+    """
+    Get a zstat file list based on sessid list and database directory.
+
+    """
+    label_list = []
+    for subj in sid_list:
+        subject_dir = os.path.join(db_dir, sid, 'face-object')
+        if not os.path.exists(subject_dir):
+            print 'Subject %s does not exist in database.'%(subj)
+            return
+        label = get_label_file(subject_dir)
+        label_list.append(label)
+    return label_list
+
+def ext_subj_sample(sid_list, zstat_file_list, mask_coord, class_label,
+                    output_dir, label_file_list=None):
+    """
+    Warper of ext_sample and save_sample for each subject.
+
+    """
+    subj_num = len(sid_list)
+    zstat_num = len(zstat_file_list)
+    if not subj_num == zstat_num:
+        print 'Inconsistency of input subject number! (zstat file list)'
+        return
+    if label_file_list:
+        label_num = len(label_list_num)
+        if not subj_num == label_num:
+            print 'Inconsistency of input subject number! (label file list)'
+            return
+
+    for i in range(subj_num):
+        subj = sid_list[i]
+        if label_file_list:
+            feature_name, sample_data = arlib.ext_sample(zstat_file_list[i],
+                                                         mask_coord,
+                                                         class_label,
+                                                         label_file_list[i])
+        else:
+            feature_name, sample_data = arlib.ext_sample(zstat_file_list[i],
+                                                         mask_coord,
+                                                         class_label)
+        out_file = os.path.join(output_dir, subj + '_data.csv')
+        arlib.save_sample(feature_name, sample_data, out_file)
+
+def get_subj_sample(sid_list, data_dir):
+    """
+    Get data for a list of subjects.
+    #sid_list# could be either a subject's SID or a SID list.
+
+    """
+    if isinstance(sid_list, str):
+        sid_list = [sid_list]
+    if not isinstance(sid_list, list):
+        print 'Invalid input!'
+        return
+    samples = np.array([])
+    for subj in sid_list:
+        f = os.path.join(data_dir, subj + '_data.csv')
+        data = np.loadtxt(f, skiprows=1, delimiter=',')
+        if not samples.size:
+            samples = data
+        else:
+            samples = np.vstack((samples, data))
+    return samples
+
+    
+
+
+db_dir = r'/nfs/t2/atlas/database'
 base_dir = r'/nfs/h1/workingshop/huanglijie/autoroi'
 doc_dir = os.path.join(base_dir, 'doc')
 data_dir = os.path.join(base_dir, 'multi-atlas', 'l_ffa_ofa')
